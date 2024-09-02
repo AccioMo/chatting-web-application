9dd1a6c2a6a13cfb545318cec5a03d3ce1bacec4
Mohammed Zeggaf <mo.zeggaf@gmail.com>
2024-09-02 13:25:45 +0100

    added login and join buttons in home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,32 @@
+import { useNavigate } from "react-router";
 import "../styles/Home.css";
 
 const Home = ({ authenticated }) => {
-	return (
-		<div className="page-content">
-			<div className="home-container">
-				<div className="welcome-container">
-					<div className="welcome">{ authenticated ? "No friends? Buy 2 to get a third COMPLETLY FREE!" : "Looks like you're not signed in..." }</div>
-				</div>
-			</div>
-		</div>
-	);
+	const nav = useNavigate();
+    return (
+        <div className="page-content">
+            <div className="home-container">
+                <div className="welcome-container">
+                    {authenticated ? (
+                        <div className="welcome">
+                            Get started by choosing a user from the sidebar and
+                            starting a chat!
+                        </div>
+                    ) : (
+                        <div className="welcome">
+                            Looks like you're not signed in...
+                            <div className="flex-center">
+                                <button className="bubble-button" onClick={() => nav('/join')}>
+                                    Sign up
+                                </button>
+                                <button className="bubble-button" onClick={() => nav('/login')}>Login</button>
+                            </div>
+                        </div>
+                    )}
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Home;
